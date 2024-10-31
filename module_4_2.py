128e48252bcee61e9ddbfb3a856e1157e0ef3086
Игорь Рябухин <igorr1970@mail.ru>
2024-10-31 21:43:10 +0500

    Update module_4_2.py

--- module_4_2.py
+++ module_4_2.py
@@ -1,15 +1,11 @@
 def test_function():
     def inner_function():
         print("Я в области видимости функции test_function")
+    inner_function()
 
-    inner_function()  # Вызов вложенной функции
-
-
-# Вызов тестовой функции
 test_function()
 
-# Попытка вызвать inner_function вне test_function
 try:
-    inner_function()  # Это вызовет ошибку
+    inner_function()
 except NameError as e:
     print(f"Ошибка: {e}")
